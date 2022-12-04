3425b216c654f00fa37af3f9f4817d1105878da1
Umar <m.umarnaeem7@gmail.com>
2022-12-04 15:11:20 +0500

    Made project pip installable

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='let-it-snow',
+    version='0.1.0',    
+    description='Create graphical snow flakes.',
+    url='https://github.com/UmarNaeem7/dsss-hw5',
+    author='Muhammad Umar Naeem',
+    author_email='umar.naeem@fau.de',
+    license='BSD 2-clause',
+    packages=['snowflake'],
+    install_requires=['numpy'],
+)
